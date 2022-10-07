46068ce695ada579f6ec53358ba18a1a6614dae6
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-10-07 22:53:39 +0300

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,65 @@
 import { Component } from 'react';
-import { Form } from './Form';
-import { Contacts } from './Contacts';
+import { ContactList } from './ContactList';
+import { ContactForm } from './ContactForm ';
+import { Filter } from './Filter';
 
 export class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: ''
-  }
+  };
+
+  searchHandler = e => {
+    this.setState({ filter: e.target });
+  };
+
+  onAddContact = data => {
+    const filtered = this.state.contacts.filter(
+      contact => contact.name === data.name
+    );
+    filtered.length
+      ? alert(`${data.name} already exists`)
+      : this.setState({
+          contacts: [data, ...this.state.contacts],
+        });
+  };
+
+  deleteContact = id => {
+    this.setState({
+      contacts: [...this.state.contacts.filter(contact => contact.id !== id)],
+    });
+  };
 
   render() {
+    const { contacts, filter } = this.state;
     return (
       <div
         style={{
-          height: '100vh',
+          minHeight: '100vh',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
+          flexDirection: 'column',
           fontSize: 40,
           color: '#010101',
+          padding: 32,
         }}
       >
-        <Form />
-        <Contacts contacts={[]} />
+        <h1 className="title">Phonebook</h1>
+        <ContactForm onAddContact={this.onAddContact} />
+
+        <h2 className="title">Contacts</h2>
+        <Filter searchHandler={this.searchHandler} filter={filter} />
+        <ContactList
+          contacts={contacts}
+          filter={filter}
+          deleteContact={this.deleteContact}
+        />
       </div>
     );
   }
